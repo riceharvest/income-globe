8ea2f256f0e466177a70ff85e7ec3fbc50947513
riceharvest <myappleiddarimaan@gmail.com>
2026-04-13 13:58:05 +0200

    update breast size data more conservative

--- app/data/breast-size-map.ts
+++ app/data/breast-size-map.ts
@@ -1,36 +1,102 @@
-// Average breast size by country (bra cup size, US standard)
+// Average breast size by country (cup only, not band)
 // Scale: 1=AA, 2=A, 3=B, 4=C, 5=D, 6=DD
-// Sources: worlddata.info, worldpopulationreview.com (2024-2025)
+// Sources: gitnux.org, bedbible.com, worlddata.info (2024-2025) - more conservative estimates
 
 export const breastSizeByCountry: Record<string, number> = {
-  // Largest: C-D to D (Nordic)
-  NO: 6, FI: 5, SE: 6, RU: 5, IS: 5, LU: 5,
+  // Largest: D (Nordic + Russia)
+  NO: 5, // Norway D
+  SE: 5, // Sweden D  
+  FI: 5, // Finland D
+  RU: 5, // Russia D
+  IS: 4, // Iceland C
+  LU: 4, // Luxembourg C
+
   // Large: C
-  US: 4, GB: 4, CO: 4, VE: 4, NL: 4, CA: 4, PL: 4, DK: 4, DE: 4, AT: 5, CH: 5, BG: 4,
+  US: 4, // USA C (some say DD but that's band+cup)
+  GB: 4, // UK C
+  CO: 4, // Colombia C
+  VE: 4, // Venezuela C
+  NL: 4, // Netherlands C
+  CA: 4, // Canada C
+  DE: 4, // Germany C
+  DK: 4, // Denmark C
+  CH: 5, // Switzerland D
+  AT: 5, // Austria D
+
+  // Medium: C
+  PL: 4, // Poland C
+  BG: 4, // Bulgaria C
+  IE: 4, // Ireland C
+  CZ: 4, // Czech C
+
+  // Medium: B-C
+  AU: 3, // Australia B-C
+  NZ: 3, // NZ B-C
+  FR: 3, // France B-C
+  ES: 3, // Spain B-C
+  IT: 3, // Italy B-C
+  GR: 3, // Greece B
+  HU: 3, // Hungary B
+
   // Medium: B
-  AU: 3, NZ: 3, FR: 3, ES: 3, PT: 3, TR: 3, GE: 3, HU: 3, IE: 4, IT: 3, GR: 3, BE: 3,
-  CL: 3, AR: 3, BR: 3, MX: 3, CZ: 4, SK: 4, RO: 4, UA: 4, RS: 4, MD: 3, HR: 3,
-  SI: 3, BA: 3, ME: 3, AL: 3, MK: 3, LT: 3, LV: 3, EE: 3, MT: 3, CY: 3,
-  // Medium-Small: A-B
-  JP: 2, CN: 2, KR: 2, TH: 2, VN: 1, IN: 2, ID: 2, MY: 2, SG: 2, PH: 2, FJ: 2,
+  BE: 3, // Belgium B
+  PT: 3, // Portugal B
+  TR: 3, // Turkey B
+  GE: 3, // Georgia B
+
+  // Smaller: B
+  BR: 3, // Brazil B
+  MX: 3, // Mexico B
+  AR: 3, // Argentina B
+  CL: 3, // Chile B
+
+  // Small: A-B
+  JP: 2, // Japan A-B
+  CN: 2, // China A
+  KR: 2, // Korea A
+  TH: 2, // Thailand A
+  VN: 1, // Vietnam AA-A
+  IN: 2, // India A
+  ID: 2, // Indonesia A
+  MY: 2, // Malaysia A
+  PH: 2, // Philippines A
+  BD: 1, // Bangladesh AA
+  NP: 1, // Nepal AA
+  MM: 1, // Myanmar AA
+
   // Small: A
-  EG: 2, SA: 2, IQ: 2, IR: 2, PK: 2, ZA: 2, MA: 2, DZ: 2, TN: 2, LY: 2, SY: 2,
-  LB: 2, JO: 2, KW: 2, QA: 2, AE: 2, BH: 2, BD: 1, NP: 1, MM: 1, KH: 1, LA: 1,
-  // Africa
-  ET: 2, NG: 2, GH: 2, KE: 2, TZ: 2, MG: 2, CM: 2, CI: 2, SN: 2, UG: 2, ZW: 2,
-  ZM: 2, MW: 2, AO: 2, CD: 2, CG: 2, BW: 2, NA: 2, SZ: 2, LS: 2,
-  // Pacific
-  WS: 1, TO: 1, VU: 1, PG: 2, SB: 2,
+  EG: 2, // Egypt A
+  SA: 2, // Saudi A
+  IQ: 2, // Iraq A
+  IR: 2, // Iran A
+  PK: 2, // Pakistan A
+  ZA: 2, // South Africa A
+  MA: 2, // Morocco A
+  DZ: 2, // Algeria A
+  TN: 2, // Tunisia A
+  LY: 2, // Libya A
+  SY: 2, // Syria A
+  LB: 2, // Lebanon A
+
+  // Africa - mostly A
+  ET: 2, NG: 2, GH: 2, KE: 2, TZ: 2, MG: 2, CM: 2,
+  CI: 2, SN: 2, UG: 2, ZW: 2, ZM: 2, MW: 2, AO: 2,
+  CD: 2, CG: 2, BW: 2, NA: 2, RW: 2, BJ: 2, TG: 2,
+  ML: 2, BF: 2, NE: 2, LR: 2, SL: 2, GM: 2, GW: 2, GN: 2,
+  
+  // Pacific  
+  FJ: 2, PG: 2, WS: 1, TO: 1, VU: 1,
+
   // Americas
-  CU: 2, DO: 2, JM: 2, HT: 2, GT: 2, HN: 2, SV: 2, NI: 2, CR: 2, PA: 2, EC: 2,
-  PE: 2, BO: 2, PY: 2, UY: 3,
-  // Central Asia
+  CU: 2, DO: 2, JM: 2, HT: 2, GT: 2, HN: 2, SV: 2, NI: 2,
+  CR: 2, PA: 2, EC: 2, PE: 2, BO: 2, PY: 2, UY: 3,
+
+  // Europe - more
+  SK: 4, RO: 4, UA: 4, RS: 4, MD: 3, HR: 3, SI: 3,
+  BA: 3, ME: 3, AL: 3, MK: 3, LT: 3, LV: 3, EE: 3,
+
+  // Central Asia  
   KZ: 4, UZ: 3, TM: 3, KG: 3, TJ: 3,
-  // South Asia
-  BT: 2, LK: 2, MV: 2,
-  // East Asia
-  MN: 3, TW: 2, HK: 2,
-  // Oceania
 };
 
 export function getBreastSize(countryCode: string): number | undefined {
